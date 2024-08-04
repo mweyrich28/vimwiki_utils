15a121e427f53090f42d767b30661a6fc928a0d1
Malte Weyrich <malte.weyrich@hotmail.de>
2024-08-04 23:51:59 +0200

    working on wiki_link

---
+++ lua/vimwikiUtils/init.lua
@@ -0,0 +1,95 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+local utils = require('vimwikiUtils.utils')
+
+local TAG_DIR = "3_tags"
+local ATOMIC_NOTES_DIR = "4_atomic_notes"
+
+local M = {}
+
+function link_to_note(child_note_wiki_path, wiki)
+    local parent_note_abs_path = vim.fn.expand("%:p")  -- get current file
+
+    local same_level = utils.same_level(parent_note_abs_path, child_note_wiki_path, wiki)
+    print(same_level)
+
+    if same_level then
+        return utils.format_md_link(utils.get_path_suffix(child_note_wiki_path))
+    end
+    
+    return utils.format_rel_md_link(child_note_wiki_path, wiki)
+end
+
+
+function M.create_new_wiki(curr_file)
+    local wiki_link = utils.format_md_link(curr_file)
+    
+    local current_dir = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h")
+    local wiki_md = current_dir .. "/" .. curr_file
+
+    local markdown_name = curr_file.match(curr_file, "[^/]+$")
+    utils.generate_header(wiki_md, markdown_name)
+    return wiki_link
+end
+
+
+function M.vimwiki_link()
+    
+    local opts = require("telescope.themes").get_dropdown { prompt_title = "VimwikiLink" }
+    
+    -- ignore tags
+    opts.file_ignore_patterns = { TAG_DIR }
+
+    -- TODO: get current wiki
+    local wiki = vim.g.vimwiki_list[1].path
+    
+    -- get all md files in wiki
+    local results = vim.fn.systemlist("find " ..  wiki .. " -type f -name '*.md'")
+    local processed_results = {}  -- table for notes to display
+    local file_map = {}  -- table for actual note paths
+
+    -- requred for formatting
+    local wiki_suffix = utils.get_path_suffix(wiki)
+
+    -- strip all results of their leading relative path
+    for _, path in ipairs(results) do
+        local markdown_file = string.match(path, ".*/" .. wiki_suffix .. "/" .. ATOMIC_NOTES_DIR .. "/(.*)")
+        if markdown_file then
+            table.insert(processed_results, markdown_file)
+            file_map[markdown_file] = string.match(path, wiki_suffix .. "/(.*)") -- map the displayed name to path relative to wiki
+        end
+    end
+
+    local note_md_name = ""
+    local wiki_link = ""
+
+    pickers.new(opts, {
+        finder = finders.new_table({
+            results = processed_results
+        }),
+        sorter = conf.file_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                local selection = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
+                note_md_name = selection.value
+                wiki_link = link_to_note(file_map[note_md_name], wiki)
+                vim.api.nvim_put({ wiki_link }, "", true, true)
+            end)
+
+            map('i', '<A-CR>', function()
+                actions.close(prompt_bufnr)
+                note_md_name = action_state.get_current_line()
+                wiki_link = M.create_new_wiki(note_md_name)
+                vim.api.nvim_put({ wiki_link }, "", true, true)
+            end)
+
+            return true
+        end
+    }):find()
+end
+
+return M
---
+++ lua/vimwikiUtils/utils.lua
@@ -0,0 +1,115 @@
+local M = {}
+
+---@param path string
+---@return table
+function M.split_path(path)
+    local parts = {}
+    -- Match any sequence of characters between slashes
+    for part in string.gmatch(path, "[^/]+") do
+        table.insert(parts, part)
+    end
+    return parts
+end
+
+
+---@param path string
+---@return number
+function M.get_depth(path)
+    local path_table = M.split_path(path)
+    local depth = 0
+    for _, _ in ipairs(path_table) do
+        depth = depth + 1
+    end
+    return depth
+end
+
+
+---@param filename string
+---@param header string
+function M.generate_header(filename, header)
+    local name_formatted = string.gsub(header, "_", " ")
+    local vimwiki_header = string.format(
+        "# %s\n> **date:** %s  \n> **tags:** \n> **material:**\n\n",
+        name_formatted,
+        os.date("%F")
+    )
+    local file = io.open(filename .. ".md", "a")
+    file:write(vimwiki_header)
+    file:close()
+end
+
+
+---@param filename string
+---@return string
+function M.format_md_link(filename)
+    local markdown_name = filename.match(filename, "[^/]+$")
+    local wiki_md_link = "[" .. string.gsub(markdown_name, "_", " ") .. "]" .. "(" .. filename.. ".md)"
+    return wiki_md_link
+end
+
+
+---@param filename string
+---@return string
+function M.format_rel_md_link(filename, wiki)
+    local parent_note_abs_path = vim.fn.expand("%:p")  -- get current file
+    local relative_path_prefix = M.gen_rel_prefix(wiki, parent_note_abs_path)
+    local formatted_name = string.gsub(string.gsub(M.get_path_suffix(filename), ".md", ""), "_", " ")  -- formatting identifier for md link: [identifier](path/to/child_note)
+    return "[" .. formatted_name .. "]" .. "(" ..relative_path_prefix .. filename.. ")"
+end
+
+
+---@param path string
+---@return string
+function M.get_path_suffix(path)
+    local path_components = M.split_path(path)
+    return path_components[#path_components]
+end
+
+
+---@param parent_note_path string
+---@param child_note_path string
+---@param wiki string
+---@return boolean
+function M.same_level(parent_note_path, child_note_path, wiki)
+    local parent_note_wiki_path = M.convert_abs_to_rel(wiki, parent_note_path)
+    
+    -- check if parent_note is located in same dir as child_note
+    local dir_child_note = child_note_path.match(child_note_path, "(.*/)")  -- remove the last part of the file identifier (md)
+    local dir_parent_note = parent_note_path.match(parent_note_wiki_path, "(.*/)")
+
+
+    if dir_child_note == dir_parent_note then
+        return true
+    end
+    
+    return false
+    
+end
+
+
+---@param wiki string
+---@param path string
+---@return string
+function M.convert_abs_to_rel(wiki, path)
+    local wiki_suffix = M.get_path_suffix(wiki)
+    local parent_note_wiki_path = string.match(path, ".*/" .. wiki_suffix .. "/(.*)")  -- gets path based on wiki: /home/usr/wiki/4_atomic_notes/note.md -> 4_atomic_notes/note.md
+    return parent_note_wiki_path
+end
+
+---@param wiki string
+---@param parent_note_abs_path string
+---@return string
+function M.gen_rel_prefix(wiki, parent_note_abs_path)
+    local curr_depht = M.get_depth(parent_note_abs_path)
+    local wiki_depth = M.get_depth(wiki)
+
+    -- Count the number of directories to go up
+    local relative_path = ""
+    for _=2, curr_depht - wiki_depth,1 do
+        relative_path = relative_path .. "../"
+    end
+
+    return relative_path
+end
+
+return M
