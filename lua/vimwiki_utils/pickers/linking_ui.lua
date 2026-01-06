c4a64c144cea999408bbd9a0e24a4f868b92cb2e
Malte Weyrich <120316140+mweyrich28@users.noreply.github.com>
2026-01-06 18:05:17 +0100

    removing duplicate code and missing link insertion

--- lua/vimwiki_utils/pickers/linking_ui.lua
+++ lua/vimwiki_utils/pickers/linking_ui.lua
@@ -45,7 +45,7 @@ function M.link_tag()
                     actions.close(prompt_bufnr)
                     notes.create_new_tag(new_tag_name, config.options.globals.tag_dir)
 
-                    local tag_file = new_tag_name .. ".md" -- TODO: maybe add func to dynamically check/add ext 
+                    local tag_file = new_tag_name .. ".md" -- TODO: maybe add func to dynamically check/add ext
                     local tag_path = vim.fs.joinpath(config.options.globals.tag_dir, tag_file)
                     local tag_link = links.format_rel_md_link(tag_path, parent_note)
                     links.put_link(tag_link)
@@ -72,7 +72,7 @@ function M.link_tag()
                 actions.close(prompt_bufnr)
                 notes.create_new_tag(new_tag_name, config.options.globals.tag_dir)
 
-                local tag_file = new_tag_name .. ".md" -- TODO: maybe add func to dynamically check/add ext 
+                local tag_file = new_tag_name .. ".md" -- TODO: maybe add func to dynamically check/add ext
                 local tag_path = vim.fs.joinpath(config.options.globals.tag_dir, tag_file)
                 local tag_link = links.format_rel_md_link(tag_path, parent_note)
                 links.put_link(tag_link)
@@ -83,155 +83,145 @@ function M.link_tag()
     }):find()
 end
 
-
 function M.link_note()
-  local opts = themes.get_dropdown {
-    prompt_title = "VimwikiUtilsLink",
-  }
+    local opts = themes.get_dropdown {
+        prompt_title = "VimwikiUtilsLink",
+    }
 
-  opts.file_ignore_patterns = {
-    config.options.globals.tag_dir,
-  }
+    opts.file_ignore_patterns = {
+        config.options.globals.tag_dir,
+    }
 
-  local globals = config.options.globals
-  local wiki = paths.get_active_wiki()
+    local globals = config.options.globals
+    local wiki = paths.get_active_wiki()
 
-  local results = vim.fn.systemlist(
-    "find " .. wiki .. " -type f -name '*.md'"
-  )
+    local results = vim.fn.systemlist(
+        "find " .. wiki .. " -type f -name '*.md'"
+    )
 
-  local processed, file_map =
-    unpack(paths.format_results(globals.atomic_notes_dir, results))
+    local processed, file_map =
+        unpack(paths.format_results(globals.atomic_notes_dir, results))
 
-  local source_file = vim.fn.expand("%:p")
+    local source_file = vim.fn.expand("%:p")
 
-  pickers.new(opts, {
-    finder = finders.new_table {
-      results = processed,
-    },
+    pickers.new(opts, {
+        finder = finders.new_table {
+            results = processed,
+        },
 
-    sorter = conf.file_sorter(opts),
+        sorter = conf.file_sorter(opts),
 
-    attach_mappings = function(prompt_bufnr, map)
-      local function paste_selected_entry()
-        local selection = action_state.get_selected_entry()
-        if not selection then return end
+        attach_mappings = function(prompt_bufnr, map)
+            local function paste_selected_entry()
+                local selection = action_state.get_selected_entry()
+                if not selection then return end
 
-        local file_name = selection.value:gsub("%.md$", "")
-        action_state
-          .get_current_picker(prompt_bufnr)
-          :reset_prompt(file_name)
-      end
+                local file_name = selection.value:gsub("%.md$", "")
+                action_state
+                    .get_current_picker(prompt_bufnr)
+                    :reset_prompt(file_name)
+            end
 
-      actions.select_default:replace(function()
-        local selection = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
+            actions.select_default:replace(function()
+                local selection = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
 
-        local note_name = ""
-        if selection then
-          note_name = selection.value
-        else
-          -- note_name = action_state.get_current_line()
-          -- local note_name_filename = action_state.get_current_line() .. ".md" -- TODO: unify
-          -- notes.create_new_note(
-          --   note_name_filename,
-          --   globals.atomic_notes_dir,
-          --   globals.tag_dir,
-          --   source_file
-          -- )
-            -- local file_path = vim.fs.joinpath(globals.atomic_notes_dir, note_name)
-            -- local parent_note = vim.fn.expand("%:p")
-            -- local wiki_link = links.format_rel_md_link(file_path, parent_note)
+                local note_name
 
+                if selection then
+                    note_name = selection.value
+                else
+                    note_name = action_state.get_current_line() .. ".md"
+                end
 
+                local file_path = vim.fs.joinpath(globals.atomic_notes_dir, note_name)
+                local parent_note = vim.fn.expand("%:p")
+                local wiki_link = links.format_rel_md_link(file_path, parent_note)
 
-            note_name = action_state.get_current_line() .. ".md"
-            local file_path = vim.fs.joinpath(globals.atomic_notes_dir, note_name)
-            local parent_note = vim.fn.expand("%:p")
-            local wiki_link = links.format_rel_md_link(file_path, parent_note)
-            links.put_link(wiki_link)
+                links.put_link(wiki_link)
 
-            notes.create_new_note(
-              note_name,
-              globals.atomic_notes_dir,
-              globals.tag_dir,
-              source_file
-            )
-        end
-      end)
+                if not selection then
+                    notes.create_new_note(
+                        note_name,
+                        globals.atomic_notes_dir,
+                        globals.tag_dir,
+                        source_file
+                    )
+                end
+            end)
 
-      map("i", "<A-CR>", function()
-        actions.close(prompt_bufnr)
+            map("i", "<A-CR>", function()
+                actions.close(prompt_bufnr)
 
-        local note_name = action_state.get_current_line() .. ".md"
-        local file_path = vim.fs.joinpath(globals.atomic_notes_dir, note_name)
-        local parent_note = vim.fn.expand("%:p")
-        local wiki_link = links.format_rel_md_link(file_path, parent_note)
-        links.put_link(wiki_link)
+                local note_name = action_state.get_current_line() .. ".md"
+                local file_path = vim.fs.joinpath(globals.atomic_notes_dir, note_name)
+                local parent_note = vim.fn.expand("%:p")
+                local wiki_link = links.format_rel_md_link(file_path, parent_note)
+                links.put_link(wiki_link)
 
-        notes.create_new_note(
-          note_name,
-          globals.atomic_notes_dir,
-          globals.tag_dir,
-          source_file
-        )
-      end)
+                notes.create_new_note(
+                    note_name,
+                    globals.atomic_notes_dir,
+                    globals.tag_dir,
+                    source_file
+                )
+            end)
 
-      map("i", "<Tab>", paste_selected_entry)
+            map("i", "<Tab>", paste_selected_entry)
 
-      return true
-    end,
-  }):find()
+            return true
+        end,
+    }):find()
 end
 
 function M.display_backlinks()
-  local current_file = vim.fn.expand("%:t:r")
-  local backlink_pattern =
-    "\\[*\\]\\(.*" .. current_file .. "[.md\\)|\\)]"
+    local current_file = vim.fn.expand("%:t:r")
+    local backlink_pattern =
+        "\\[*\\]\\(.*" .. current_file .. "[.md\\)|\\)]"
 
-  local results = vim.fn.systemlist(
-    "rg --vimgrep " .. vim.fn.shellescape(backlink_pattern)
-  )
+    local results = vim.fn.systemlist(
+        "rg --vimgrep " .. vim.fn.shellescape(backlink_pattern)
+    )
 
-  pickers.new({}, {
-    prompt_title = "VimwikiUtilsBacklinks",
-    finder = finders.new_table {
-      results = results,
-      entry_maker = function(entry)
-        local file, lnum, col, text =
-          entry:match("^(.-):(%d+):(%d+):(.*)$")
+    pickers.new({}, {
+        prompt_title = "VimwikiUtilsBacklinks",
+        finder = finders.new_table {
+            results = results,
+            entry_maker = function(entry)
+                local file, lnum, col, text =
+                    entry:match("^(.-):(%d+):(%d+):(.*)$")
 
-        if not file then return nil end
+                if not file then return nil end
 
-        return {
-          value = entry,
-          filename = file,
-          lnum = tonumber(lnum),
-          col = tonumber(col),
-          text = text,
-          display = function()
-            return paths.get_path_suffix(file) .. ": [l." .. lnum .."]"
-          end,
-          ordinal = file .. " " .. text,
-        }
-      end,
-    },
+                return {
+                    value = entry,
+                    filename = file,
+                    lnum = tonumber(lnum),
+                    col = tonumber(col),
+                    text = text,
+                    display = function()
+                        return paths.get_path_suffix(file) .. ": [l." .. lnum .. "]"
+                    end,
+                    ordinal = file .. " " .. text,
+                }
+            end,
+        },
 
-    sorter = conf.generic_sorter({}),
+        sorter = conf.generic_sorter({}),
 
-    previewer = conf.grep_previewer({}),
+        previewer = conf.grep_previewer({}),
 
-    attach_mappings = function(prompt_bufnr)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local entry = action_state.get_selected_entry()
+        attach_mappings = function(prompt_bufnr)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local entry = action_state.get_selected_entry()
 
-        vim.cmd.edit(entry.filename)
-        vim.api.nvim_win_set_cursor(0, { entry.lnum, entry.col - 1 })
-      end)
-      return true
-    end,
-  }):find()
+                vim.cmd.edit(entry.filename)
+                vim.api.nvim_win_set_cursor(0, { entry.lnum, entry.col - 1 })
+            end)
+            return true
+        end,
+    }):find()
 end
 
 return M
